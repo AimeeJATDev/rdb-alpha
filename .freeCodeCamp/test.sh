5c257a76d783a1cfeab65696f32cacb7eacf3869
Aimee Taylor <114760316+AimeeJATDev@users.noreply.github.com>
2026-05-29 15:47:11 -0400

    Save progress: 940.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Bingo Number Generator
 
-NUMBER=5
+NUMBER=$((RANDOM % 75 + 1))
+TEXT="The next number is, "
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
 echo $NUMBER
+
+echo $TEXT
